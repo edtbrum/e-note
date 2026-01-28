8213b2cc9b06c68b978b33085fbddf7cb72bdeaa
Eduardo Brum <edtbrum@gmail.com>
2026-01-28 19:42:14 -0300

    Classe cConnectionMySQL

---
+++ backend/src/dao/cConnectionMySQL.cpp
@@ -0,0 +1,46 @@
+#include "cConnectionMySQL.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <stdexcept>
+#include <string>
+
+cConnectionMySQL::cConnectionMySQL() {
+    std::string hostname = "tcp://127.0.0.1:3306";
+    std::string username = "dev";
+    std::string password = "password";
+    std::string schema = "db_rentalcar";
+
+    try {
+        sql::Driver *driver = get_driver_instance();
+        m_conn.reset(driver->connect(hostname,username,password));
+        m_conn->setSchema(schema);
+    }
+    catch (const sql::SQLException& e) {
+        throw std::runtime_error("Error: " + std::string(e.what()));
+    }
+}
+
+cConnectionMySQL::~cConnectionMySQL() {
+    m_conn->close();
+}
+
+sql::Connection* cConnectionMySQL::connection() noexcept {
+    return m_conn ? m_conn.get() : nullptr;
+}
+
+bool cConnectionMySQL::isConnected() const {
+    return m_conn->isValid();
+}
+
+void cConnectionMySQL::reconnect() {
+    if (this->isConnected()) { return; }
+
+    try {
+        if (m_conn && m_conn->reconnect()) {
+            m_conn->setSchema("db_rentalcar");
+        }
+    }
+    catch (const sql::SQLException& e) {
+        throw std::runtime_error("Error: " + std::string(e.what()));
+    }
+}
---
+++ backend/src/dao/cConnectionMySQL.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cppconn/connection.h>
+#include <memory>
+
+class cConnectionMySQL {
+public:
+    cConnectionMySQL();
+    ~cConnectionMySQL();
+
+    sql::Connection* connection() noexcept;
+
+    bool isConnected() const;
+    void reconnect();
+private:
+    std::unique_ptr<sql::Connection> m_conn;
+};
