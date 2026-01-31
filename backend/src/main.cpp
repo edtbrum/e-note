00fd6ab3cc8ee04b5021a940d63b2f8ad451c2ea
Eduardo Brum <edtbrum@gmail.com>
2026-01-31 19:09:46 -0300

    Teste do controller no Postman - OK

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,6 +1,20 @@
-#include <iostream>
+#include "controller/autorController.h"
+#include "controller/sCorsMiddleware.h"
+#include "dao/cAutorDAO.h"
+#include "dao/cConnectionMySQL.h"
+#include <crow.h>
 
 int main() {
-    std::cout << ">> e-note project" << std::endl;
+    crow::App<CorsMiddleware> app;
+    cConnectionMySQL conn;
+    cAutorDAO autor_dao(conn);
+
+    // Author Services
+    registerCreateAutorRoutes(app, autor_dao);
+    registerUpdateAutorRoutes(app, autor_dao);
+    registerDeleteAutorRoutes(app, autor_dao);
+    registerListAutoresRoutes(app, autor_dao);
+
+    app.port(18080).multithreaded().run();
     return 0;
 }
